8d80ff298bae97a9fa61767618c40320d63ba216
Farid118 <faridmasjedi118@gmail.com>
2022-01-01 20:37:01 +1100

    Add : All the CRUD methods

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ class CreateFolderAndFile {
           fs.mkdirSync(dir, {
             recursive: true
           })
-          return dir;
         } else {
-          throw new Error(`\n-----------\n"${dir}" is existed already!!!\n---------------\n`);
+        //   throw new Error(`\n-----------\n"${dir}" is existed already!!!\n---------------\n`);
+            console.log(`the ${dir} database is already exist.`)
         }
+        return dir;
     }
       
-    makeAFile = (path, fileType, data) => {
-        fs.appendFileSync(`./${path}.${fileType}`, data, (err) => {
+    makeAFile = (path, filename, fileType, data) => {
+        fs.appendFileSync(`./${path}/${filename}.${fileType}`, data, (err) => {
         if (err) throw 'appending error: ' + err;
         })
     }
@@ -46,52 +47,160 @@ class CreateFolderAndFile {
     
         let fileToSearch = folderToSearch+'/';
         fileToSearch += files.find( file => {
-        let data = fs.readFileSync(folderToSearch+'/' + file, 'utf-8');
-        data = data.toString();
-        let condition = data.includes(fileContentToSearch);
-        return condition;
+            let data = fs.readFileSync(folderToSearch+'/' + file, 'utf-8');
+            data = data.toString();
+            let condition = data.includes(fileContentToSearch);
+            return condition;
         })
         return fileToSearch.includes('undefined') ? `\nNo file contain "${fileContentToSearch}".\n` : fileToSearch;
     }
 
+    searchInAFile = (filePath, fileContentToSearch) => {
+        let data = fs.readFileSync(filePath, 'utf-8');
+        data = data.toString();
+        let condition = data.includes(fileContentToSearch);
+        return condition;
+    }
+
+    appenToIdFile = (filePath, contentObj) => {
+        let data = fs.readFileSync(filePath, 'utf-8');
+        if (data) { data = JSON.parse(data); }
+
+        let contentTobeAppended = data ? {...data} : {};
+        contentTobeAppended[contentObj['Id']] = contentObj;
+
+        let content = JSON.stringify(contentTobeAppended)
+        fs.writeFileSync(filePath, content , function (err) {
+            if (err) throw err;
+            console.log( 'Saved!' );
+        });
+    }
+
+    appendToAFile = (filePath, contentTobeAppended, id) => {
+        let data = fs.readFileSync(filePath, 'utf-8');
+        data = data.toString();
+        let content = id + ' : ' + contentTobeAppended;
+        fs.appendFile(filePath, !data.length ? content : ',\n' + content, function (err) {
+            if (err) throw err;
+            console.log('Saved!' );
+        });
+    }
+
+    appenToAFileDiffKey = (folderPath, columnsArray, contentObj) => {
+        
+        let columns = [...columnsArray];
+        columnsArray.map( c => {
+            if (c !== 'Id') {
+                this.makeAFile(folderPath, c + '-Id', 'txt', '');
+                columns.push(c + '-Id')
+                return c + '-Id'
+            }
+        })
+
+        columns.forEach(c => {
+            let filePath = folderPath + '/' + c + '.txt';
+            let data = fs.readFileSync(filePath, 'utf-8');
+            if (data) { data = JSON.parse(data); }
+
+            let contentTobeAppended = data ? {...data} : {};
+            if (c === 'Id') {
+                contentTobeAppended[contentObj[c]] = contentObj
+            } else if (c.includes('-Id')) {
+                contentTobeAppended[contentObj[c.split('-Id')[0]] + '-' +contentObj['Id']] = contentObj;
+            } else {
+                if (contentTobeAppended[contentObj[c]]) {
+                    contentTobeAppended[contentObj[c]][contentObj['Id']] = contentObj
+                }else{
+                    contentTobeAppended[contentObj[c]] = {}
+                    contentTobeAppended[contentObj[c]][contentObj['Id']] = contentObj
+                }  
+            }
+            
+            let ctn = JSON.stringify(contentTobeAppended)
+
+            fs.writeFileSync(filePath, ctn , function (err) {
+                if (err) throw err;
+                console.log( `Saved into ${c} table.` );
+            });
+            
+        })
+        
+    }
+
+    updateAFile = (filepath, data) => {
+        if (typeof data !== 'string') { data = JSON.stringify(data) }
+        fs.writeFileSync(filepath, data , function (err) {
+            if (err) throw err;
+            console.log( `Update ${c} table.` );
+        }); 
+    }
+
+    searchInJSON = (filepath, contentColumns, contentArray) => {
+        let data = fs.readFileSync(filepath, 'utf-8');
+        data = data.toString();
+
+        let str = '';
+
+        contentColumns.forEach((c,index) => {
+            let value = contentArray[index];
+
+            if (c !== 'Id') {
+                str += '"' +c+ '"' + ':' + (typeof value === 'string' ? '"' +value+ '"' : value) + ',';
+            }
+        })
+        str = str.slice(0,-1);
+        return data.includes(str);
+    }
+
+    readFileToString = (filepath) => {
+        let data = fs.readFileSync(filepath, 'utf-8');
+        return data.toString();
+    }
+
+    readAllInfoToJSON = (filepath) => {
+        let data = fs.readFileSync(filepath, 'utf-8');
+        return JSON.parse(data);
+    }
 }
 
 
-let createFolderAndFile = new CreateFolderAndFile();
-createFolderAndFile.createAFolder('./newFolder');
+// let createFolderAndFile = new CreateFolderAndFile();
+// createFolderAndFile.createAFolder('./newFolder');
 
-let files;
-console.log('\n\n-----\n\n');
-setTimeout(() => createFolderAndFile.makeAFile('./newFolder/newFile', 'txt' , '----Just a testing text----'), 400);
+// let files;
+// console.log('\n\n-----\n\n');
+// setTimeout(() => createFolderAndFile.makeAFile('./newFolder','newFile', 'txt' , '----Just a testing text----'), 400);
 
-setTimeout(() => {
-    files = createFolderAndFile.getAllFilesFromDir('./newFolder');
-    console.log(files)
-    console.log('\n\n-----\n\n');
-}, 400);
+// setTimeout(() => {
+//     files = createFolderAndFile.getAllFilesFromDir('./newFolder');
+//     console.log(files)
+//     console.log('\n\n-----\n\n');
+// }, 400);
 
-setTimeout(() => {
-    let folder = createFolderAndFile.findTheProperFolder('./newFolder', 'newFile');
-    console.log(folder);
-    console.log('\n\n-----\n\n');
+// setTimeout(() => {
+//     let folder = createFolderAndFile.findTheProperFolder('./newFolder', 'newFile');
+//     console.log(folder);
+//     console.log('\n\n-----\n\n');
 
-    folder = createFolderAndFile.findTheProperFolder('./newFolder', 'NotExisted');
-    console.log(folder);
-    console.log('\n\n-----\n\n');
+//     folder = createFolderAndFile.findTheProperFolder('./newFolder', 'NotExisted');
+//     console.log(folder);
+//     console.log('\n\n-----\n\n');
 
-}, 400);
+// }, 400);
 
-setTimeout(() => {
-    let file = createFolderAndFile.findTheProperFile('./newFolder', 'Just a testing text');
-    console.log(file);
-    console.log('\n\n-----\n\n');
+// setTimeout(() => {
+//     let file = createFolderAndFile.findTheProperFile('./newFolder', 'Just a testing text');
+//     console.log(file);
+//     console.log('\n\n-----\n\n');
 
-    file = createFolderAndFile.findTheProperFile('./newFolder', 'Error');
-    console.log(file);
-    console.log('\n\n-----\n\n');
+//     file = createFolderAndFile.findTheProperFile('./newFolder', 'Error');
+//     console.log(file);
+//     console.log('\n\n-----\n\n');
 
-    file = createFolderAndFile.findTheProperFile('./noFolder', 'Just a testing text');
-    console.log(file);
-    console.log('\n\n-----\n\n');
+//     file = createFolderAndFile.findTheProperFile('./noFolder', 'Just a testing text');
+//     console.log(file);
+//     console.log('\n\n-----\n\n');
 
-}, 400)
+// }, 400)
+
+module.exports = CreateFolderAndFile;
